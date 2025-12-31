9022f6c612d638a265b158b44bc07a6fdc30f546
Sahil <sahilnagel@gmail.com>
2025-12-31 13:04:14 +0530

    added safe types

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 //basic express boilerplate;
 const express = require('express');
+import { createTodo, updateTodo } from "./types";
 const app = express();
+app.use(express.json());
 
+
+app.post("/todo", (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "you sent wrong inputs",
+    })
+    return;
+  }
+})
+
+app.get("/todo", (req, res) => {
+
+})
 const port = 3000;
 
+app.put("/completed", (req, res) => {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(parsedPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "you sent the wrong inputs",
+    })
+    return;
+  }
 
-app.get('/', (req, res) => {
-  res.send("hello world");
 })
+
 
 app.listen(port, () => {
   console.log(`backend running on port ${port}`)
